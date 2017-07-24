59d214392d8997fc088c15c8ed68ca78628311ae
Rickard Laurin <rickard.laurin@gmail.com>
2017-07-24 21:33:37 +0200

    ✅ Fixes #2 - Migrate tests to Jest

--- test/numberingArea.spec.js
+++ test/numberingArea.spec.js
@@ -1,35 +1,34 @@
-const { expect } = require('chai')
 const numberingArea = require('../lib/numberingArea')
 
 describe('#numberingArea', () => {
   it('returns empty string if nothing is provided', () => {
-    expect(numberingArea()).to.eql('')
+    expect(numberingArea()).toEqual('')
   })
 
   it('returns empty string if nothing is found', () => {
-    expect(numberingArea('1234')).to.eql('')
+    expect(numberingArea('1234')).toEqual('')
   })
 
   it('returns a numbering area for a given area code', () => {
-    expect(numberingArea('08')).to.eql('Stockholm')
+    expect(numberingArea('08')).toEqual('Stockholm')
   })
 
   it('returns numbering area for a more complex area', () => {
-    expect(numberingArea('0142')).to.eql('Mjölby-Skänninge-Boxholm')
+    expect(numberingArea('0142')).toEqual('Mjölby-Skänninge-Boxholm')
   })
 
   it('returns correctly when provided a number', () => {
-    expect(numberingArea(8)).to.eql('Stockholm')
-    expect(numberingArea(142)).to.eql('Mjölby-Skänninge-Boxholm')
+    expect(numberingArea(8)).toEqual('Stockholm')
+    expect(numberingArea(142)).toEqual('Mjölby-Skänninge-Boxholm')
   })
 
   it('if provided a real phonenumber finds the area', () => {
-    expect(numberingArea('081234567')).to.eql('Stockholm')
-    expect(numberingArea('031124567')).to.eql('Göteborg')
-    expect(numberingArea('0977-123 45')).to.eql('Korpilombolo')
+    expect(numberingArea('081234567')).toEqual('Stockholm')
+    expect(numberingArea('031124567')).toEqual('Göteborg')
+    expect(numberingArea('0977-123 45')).toEqual('Korpilombolo')
   })
 
   it('does not use long number if not a parseable number', () => {
-    expect(numberingArea('08123456712312')).to.eql('')
+    expect(numberingArea('08123456712312')).toEqual('')
   })
 })
--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,73 +1,72 @@
-const { expect } = require('chai')
 const phoneNumberParser = require('../lib/parser')
 
 describe('#phoneNumberParser', () => {
   it('handles null values', () => {
-    expect(phoneNumberParser(null)).to.eql('')
-    expect(phoneNumberParser(undefined)).to.eql('')
+    expect(phoneNumberParser(null)).toEqual('')
+    expect(phoneNumberParser(undefined)).toEqual('')
   })
 
   it('parses voicemail', () => {
-    expect(phoneNumberParser('46888')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('46888')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('46333')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('46147')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('888')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('333')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('222')).to.eql('Röstbrevlåda')
-    expect(phoneNumberParser('147')).to.eql('Röstbrevlåda')
+    expect(phoneNumberParser('46888')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('46888')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('46333')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('46147')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('888')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('333')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('222')).toEqual('Röstbrevlåda')
+    expect(phoneNumberParser('147')).toEqual('Röstbrevlåda')
   })
 
   it('handles non strings', () => {
-    expect(phoneNumberParser(46721834008)).to.eql('072-183 40 08')
+    expect(phoneNumberParser(46721834008)).toEqual('072-183 40 08')
   })
 
   it('parses normal mobile numbers', () => {
-    expect(phoneNumberParser('46721834008')).to.eql('072-183 40 08')
-    expect(phoneNumberParser('0721834008')).to.eql('072-183 40 08')
-    expect(phoneNumberParser('0701234567')).to.eql('070-123 45 67')
+    expect(phoneNumberParser('46721834008')).toEqual('072-183 40 08')
+    expect(phoneNumberParser('0721834008')).toEqual('072-183 40 08')
+    expect(phoneNumberParser('0701234567')).toEqual('070-123 45 67')
   })
 
   it('parses landline numbers with 2 number area codes', () => {
-    expect(phoneNumberParser('4681234567')).to.eql('08-123 45 67')
-    expect(phoneNumberParser('081234567')).to.eql('08-123 45 67')
-    expect(phoneNumberParser('468123456')).to.eql('08-12 34 56')
-    expect(phoneNumberParser('08123456')).to.eql('08-12 34 56')
+    expect(phoneNumberParser('4681234567')).toEqual('08-123 45 67')
+    expect(phoneNumberParser('081234567')).toEqual('08-123 45 67')
+    expect(phoneNumberParser('468123456')).toEqual('08-12 34 56')
+    expect(phoneNumberParser('08123456')).toEqual('08-12 34 56')
   })
 
   it('parses landline numbers with 2 digits area codes and 8 number chars', () => {
-    expect(phoneNumberParser('46812345678')).to.eql('08-123 456 78')
+    expect(phoneNumberParser('46812345678')).toEqual('08-123 456 78')
   })
 
   it('parses landline number with 3 number area codes', () => {
-    expect(phoneNumberParser('03112345')).to.eql('031-123 45')
-    expect(phoneNumberParser('031446572')).to.eql('031-44 65 72')
-    expect(phoneNumberParser('4631626262')).to.eql('031-62 62 62')
-    expect(phoneNumberParser('031626262')).to.eql('031-62 62 62')
-    expect(phoneNumberParser('46311234567')).to.eql('031-123 45 67')
-    expect(phoneNumberParser('0311234567')).to.eql('031-123 45 67')
+    expect(phoneNumberParser('03112345')).toEqual('031-123 45')
+    expect(phoneNumberParser('031446572')).toEqual('031-44 65 72')
+    expect(phoneNumberParser('4631626262')).toEqual('031-62 62 62')
+    expect(phoneNumberParser('031626262')).toEqual('031-62 62 62')
+    expect(phoneNumberParser('46311234567')).toEqual('031-123 45 67')
+    expect(phoneNumberParser('0311234567')).toEqual('031-123 45 67')
   })
 
   it('parses landline number with 4 number area codes', () => {
-    expect(phoneNumberParser('050012345')).to.eql('0500-123 45')
-    expect(phoneNumberParser('46304123456')).to.eql('0304-12 34 56')
-    expect(phoneNumberParser('0304123456')).to.eql('0304-12 34 56')
-    expect(phoneNumberParser('46500123456')).to.eql('0500-12 34 56')
-    expect(phoneNumberParser('0500123456')).to.eql('0500-12 34 56')
+    expect(phoneNumberParser('050012345')).toEqual('0500-123 45')
+    expect(phoneNumberParser('46304123456')).toEqual('0304-12 34 56')
+    expect(phoneNumberParser('0304123456')).toEqual('0304-12 34 56')
+    expect(phoneNumberParser('46500123456')).toEqual('0500-12 34 56')
+    expect(phoneNumberParser('0500123456')).toEqual('0500-12 34 56')
   })
 
   it('parses numbers with +46', () => {
-    expect(phoneNumberParser('+46701234567')).to.eql('070-123 45 67')
+    expect(phoneNumberParser('+46701234567')).toEqual('070-123 45 67')
   })
 
   it('parses numbers with complex internationalization', () => {
-    expect(phoneNumberParser('+46 (0) 701234567')).to.eql('070-123 45 67')
+    expect(phoneNumberParser('+46 (0) 701234567')).toEqual('070-123 45 67')
   })
 
   it('can parse with custom separator', () => {
-    expect(phoneNumberParser('031446572', ' <> ')).to.eql('031 <> 44 65 72')
-    expect(phoneNumberParser('0701234567', ' ')).to.eql('070 123 45 67')
-    expect(phoneNumberParser('0701234567', ':')).to.eql('070:123 45 67')
-    expect(phoneNumberParser('050012345', ' ')).to.eql('0500 123 45')
+    expect(phoneNumberParser('031446572', ' <> ')).toEqual('031 <> 44 65 72')
+    expect(phoneNumberParser('0701234567', ' ')).toEqual('070 123 45 67')
+    expect(phoneNumberParser('0701234567', ':')).toEqual('070:123 45 67')
+    expect(phoneNumberParser('050012345', ' ')).toEqual('0500 123 45')
   })
 })
