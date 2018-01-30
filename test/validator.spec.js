ba8406132fdeecfb6fcbf7bdf51563d4545698e8
believer <rickard.laurin@gmail.com>
2018-01-30 19:10:52 +0100

    Update tests, minor code style adjustments

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,54 +1,86 @@
 const validator = require('../lib/validator')
 
+function validateHelper(numbers, expected) {
+  numbers.forEach(number => {
+    expect(validator(number)).toBe(expected)
+  })
+}
+
 describe('#validator', () => {
   it('returns false when nothing is provided', () => {
     expect(validator()).toBe(false)
   })
 
+  it('returns false for non-numerical input', () => {
+    const numbers = ['abcdefg', '070abcdefb', '070-abc de fg']
+
+    validateHelper(numbers, false)
+  })
+
   it('returns true for valid mobile numbers', () => {
-    expect(validator('0701234567')).toBe(true)
-    expect(validator('0721234567')).toBe(true)
-    expect(validator('0731234567')).toBe(true)
-    expect(validator('0761234567')).toBe(true)
-    expect(validator('0791234567')).toBe(true)
-    expect(validator('070-123 45 67')).toBe(true)
+    const numbers = [
+      '0701234567',
+      '0721234567',
+      '0731234567',
+      '0761234567',
+      '0791234567',
+      '070-123 45 67'
+    ]
+
+    validateHelper(numbers, true)
   })
 
   it('returns false for invalid mobile numbers', () => {
-    expect(validator('0711234567')).toBe(false)
-    expect(validator('070123456')).toBe(false)
-    expect(validator('071-123 45 67')).toBe(false)
+    const numbers = ['0711234567', '070123456', '071-123 45 67']
+
+    validateHelper(numbers, false)
   })
 
   it('returns correct for 2 digit area codes', () => {
-    expect(validator('081234567')).toBe(true)
-    expect(validator('08123456')).toBe(true)
-    expect(validator('08-123 45 67')).toBe(true)
-    expect(validator('08-12 34 56')).toBe(true)
+    const validNumbers = [
+      '081234567',
+      '08123456',
+      '08-123 45 67',
+      '08-12 34 56'
+    ]
 
-    expect(validator('0812345678')).toBe(false)
-    expect(validator('0812345')).toBe(false)
-    expect(validator('08-123 67')).toBe(false)
-    expect(validator('08-12 34 5611')).toBe(false)
+    const invalidNumbers = [
+      '0812345678',
+      '0812345',
+      '08-123 67',
+      '08-12 34 5611'
+    ]
+
+    validateHelper(validNumbers, true)
+    validateHelper(invalidNumbers, false)
   })
 
   it('returns correct for 3 digit area codes', () => {
-    expect(validator('03112345')).toBe(true)
-    expect(validator('031446572')).toBe(true)
-    expect(validator('4631626262')).toBe(true)
-    expect(validator('031626262')).toBe(true)
-    expect(validator('46311234567')).toBe(true)
-    expect(validator('0311234567')).toBe(true)
+    const validNumbers = [
+      '03112345',
+      '031446572',
+      '4631626262',
+      '031626262',
+      '46311234567',
+      '0311234567'
+    ]
 
-    expect(validator('0104465721')).toBe(false)
+    const invalidNumbers = ['0104465721']
+
+    validateHelper(validNumbers, true)
+    validateHelper(invalidNumbers, false)
   })
 
   it('returns correct for 4 digit area codes', () => {
-    expect(validator('050012345')).toBe(true)
-    expect(validator('46304123456')).toBe(true)
-    expect(validator('0304123456')).toBe(true)
-    expect(validator('46500123456')).toBe(true)
-    expect(validator('0500123456')).toBe(true)
+    const numbers = [
+      '050012345',
+      '46304123456',
+      '0304123456',
+      '46500123456',
+      '0500123456'
+    ]
+
+    validateHelper(numbers, true)
   })
 
   it('returns false for any other case', () => {
