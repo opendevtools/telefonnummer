dfbdfabe78caf61f9d3ed63eb30efa1cc8ee21b6
Rickard Laurin <rickard.laurin@gmail.com>
2018-02-04 14:56:51 +0100

    Add everything to default export

--- lib/__tests__/index.spec.ts
+++ lib/__tests__/index.spec.ts
@@ -1,7 +1,19 @@
-import { areaCode, numberingArea, parse, validator } from '../index'
+import telefonnummer, { areaCode, numberingArea, parse, validator } from '../index'
 
 describe('exports', () => {
-  it('should work as advertised', () => {
+  it('should handle default export', () => {
+    const parserResult = telefonnummer.parse('0701234567')
+    const areaCodeResult = telefonnummer.areaCode('Göteborg')
+    const numberingAreaResult = telefonnummer.numberingArea('08')
+    const validatorResult = telefonnummer.validator('0701234567')
+
+    expect(parserResult).toEqual('070-123 45 67')
+    expect(areaCodeResult).toEqual('031')
+    expect(numberingAreaResult).toEqual('Stockholm')
+    expect(validatorResult).toBe(true)
+  })
+
+  it('should handle named exports', () => {
     const parserResult = parse('0701234567')
     const areaCodeResult = areaCode('Göteborg')
     const numberingAreaResult = numberingArea('08')
--- lib/index.ts
+++ lib/index.ts
@@ -8,3 +8,11 @@ export const riktnummer = riktnummerOriginal
 export const numberingArea = numberingAreaOriginal
 export const parse = parseOriginal
 export const validator = validatorOriginal
+
+export default {
+  areaCode: findMatchingAreaName,
+  numberingArea: numberingAreaOriginal,
+  parse: parseOriginal,
+  riktnummer: riktnummerOriginal,
+  validator: validatorOriginal,
+}
